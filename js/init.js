9b06c187b0e8ce35a397f9da163d6ca9df981f13
Jose Rodriguez <jusrodriguezuy@gmail.com>
2020-08-16 03:36:46 -0300

    Fixed an infinite loop when a player was log in

--- js/init.js
+++ js/init.js
@@ -40,9 +40,11 @@ var getJSONData = function(url){
     });
 }
 
-if (!window.location.href.endsWith('login.html')) {
+if (
+    !window.location.href.endsWith('login.html') &&
+    !sessionStorage.getItem('logueado')) {
   window.location.href = 'login.html'
-}
+}  
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
--- js/login.js
+++ js/login.js
@@ -1,8 +1,8 @@
 function submitEventHandler(evento) {
     evento.preventDefault();
-    // window.location.href = 'index.html';
+    sessionStorage.setItem('logueado', true);
+    window.location.href = 'index.html';
 }
-console.log()
 
 document.getElementById('formulario-login').addEventListener('submit', submitEventHandler)
 //Función que se ejecuta una vez que se haya lanzado el evento de
