aa0b2ce4eb55d004b19429864bdb9a382d4bc448
Jose Rodriguez <jusrodriguezuy@gmail.com>
2020-08-16 03:28:57 -0300

    Fixed an infinite loop when the user was log in

---
+++ js/login.js
@@ -0,0 +1,13 @@
+function submitEventHandler(evento) {
+    evento.preventDefault();
+    // window.location.href = 'index.html';
+}
+console.log()
+
+document.getElementById('formulario-login').addEventListener('submit', submitEventHandler)
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function(e){
+
+});
